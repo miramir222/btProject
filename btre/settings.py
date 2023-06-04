5bc2bd58e52ae11fa05dd0fb549c3b3f0fb7e9e7
miramir222 <30383998+miramir222@users.noreply.github.com>
2023-06-04 09:09:58 +0530

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -139,8 +139,8 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'amir@anviam.com'
-EMAIL_HOST_PASSWORD = 'anviam@12@12'
+EMAIL_HOST = ''
+EMAIL_PORT = 
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS= True
